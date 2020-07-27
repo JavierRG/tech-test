df7e099de455454be17ef7aa1a711263dc543d6e
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 15:43:10 -0500

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,16 +1,21 @@
 """
 Your module documentation here
 """
-
-
 class PrimeClass(object):
     """
     Your class documentation here
     """
-
     def is_prime(self, num_int):
         """
         Your method documentation here
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int <  2:
+            return False
+        for i in range(2, num_int):
+            if num_int % i == 0:
+                return False
+        return True
+valor = int(input("Introduzca un numero: "))
+primaro = PrimeClass()
+primaro.is_prime(valor)
