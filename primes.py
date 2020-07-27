0e31233bf3ad6a63ec77aae413ca0d101491d353
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 16:42:01 -0500

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,22 +1,22 @@
 """
-Your module documentation here
+
 Modulo de insercion de valores en donde se valida si son numeros primarios o no.
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+ 
     Clase de inicializacion de funciones para validacion de datos.
     """
 
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+
         La funcion is_prime requiere un valor para determinar si es un numero
-         primario o si no lo es. Condicionando que un número primo es un
-          número que solo es divisible por sí solo y por 1.
+        primario o si no lo es. Condicionando que un número primo es un
+        número que solo es divisible por sí solo y por 1.
         
         """
         # your primes code here
