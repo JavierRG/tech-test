0e741c7407ff23b8a745009b6fdbca68840fb5ca
JavierRG <37031230+JavierRG@users.noreply.github.com>
2020-07-27 19:37:34 -0500

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -1,15 +1,29 @@
--- Your sql code in this file
--- NOTE: Please, don't add sentence to create database in this script file.
---       You can create database locally to test it.
---       Consider add ';' at end sentence.
-
-CREATE TABLE employee (
-);
-
-CREATE TABLE employee_department (
-);
-
 CREATE TABLE employee_hobby (
+    id int IDENTITY(1,1) PRIMARY KEY,
+    name varchar(255),
+    description varchar(255)
 );
 
--- ...
+CREATE TABLE hobbys (
+    id int NOT NULL PRIMARY KEY,
+    Employee_ID int,
+    FOREIGN KEY (Employee_ID) REFERENCES employee(id),
+    Hobby_ID int,
+    FOREIGN KEY (Hobby_ID) REFERENCES employee_hobby(id)
+);
+
+INSERT INTO employee_hobby VALUES
+(1, 'Programacion', 'Sistemas'),
+(2, 'Futbol', 'Deporte'),
+(3, 'Ejercicio', 'Gimnasio');
+
+INSERT INTO hobbys VALUES
+('1','1'),
+('1','2'),
+('1','3'),
+('2','1'),
+('2','3');
+
+
+
+
